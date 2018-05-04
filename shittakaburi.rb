57a02663b550988f5d41c15488e7b9bd662e1b8a
tapaboy <tapaboy.shadowmoon@gmail.com>
2018-05-04 11:32:51 +0900

    「知ったかぶり」機能搭載ほか -知ったかぶり機能搭載 -siritoriのファイル名変更 -gitignore作成（YAMLファイルをアップしないように）

--- shittakaburi.rb
+++ shittakaburi.rb
@@ -1,28 +1,25 @@
-#### 使う配列とリストを定義
-dictionary={}
-name_list=[]
-dicto=[]
-#### ファイルから読み込む
-File.open("dictionary.txt","r") do |dict|
-  dict.each_line do |line|
-    ## ,で分けて、前をハッシュのキーに、後を値として読み込む
-    ## また、nameだけで配列を作る。
-    dicto.push(line)
-    puts dicto
-    name="仮の名"
-    mean="仮の意味"
-    dictionary[name]=mean
-    name_list.push(name)
+require "yaml"
+require "pp"
+
+
+class Sittakaburi
+  def initialize
+    begin
+      @dictionary=YAML.load_file"dictionary.yaml"
+    rescue
+      puts "嫁>言葉を教えてあげようと思ったのだけれど、私の知っている言葉はただひとつ「AIしている？」だけ。"
+      exit
+    end
+    ## ハッシュのキーを配列として取り出す。
+    @wordslist=@dictionary.keys
+  end
+
+  def teach
+    @word=@wordslist[rand(@wordslist.size)]
+    puts "嫁>ねえ、「"+@word+"」の意味知ってる？"
+    puts "嫁>それは「"+@dictionary[@word]+"」という意味なのよ。私って物知りでしょ？"
   end
 end
 
-#### 意味を教えようとする
-select=rand(name_list.size)
-selected_word=name_list[select]
-
-puts "ねえ、「" + selected_word + "」の意味教えてあげましょうか？"
-
-print "うん、教えて(Y)  知ってるからいいよ(N)"
-gets.chomp
-
-puts "「" + selected_word + "」は、「" + dictionary[selected_word] + "」という意味なのよ。知ってた？"
+sittakaburi=Sittakaburi.new
+sittakaburi.teach
