db8cef7feea200a84d0e9a6f29ca707d8ca51dab
tapaboy <tapaboy.shadowmoon@gmail.com>
2018-05-19 20:40:54 +0900

    meimeiをメソッド化して、greetingの例外処理で実行できるようにした。

--- greeting.rb
+++ greeting.rb
@@ -1,16 +1,33 @@
 require 'yaml'
 require 'pp'
+require './meimei'
+require './shiritori'
+require './learning'
+require './shiritori_learning'
+require './shittakaburi'
 
 class Greeting
   def initialize
     begin
-      names=YAML.load_file'NAMES.yaml'
-      puts names['master'].chomp + '、'+names['yome'].chomp+'をお呼びですか？'
-      pp @names
+      @names=YAML.load_file'NAMES.yaml'
+      puts @names['yome']+'> '+@names['master'] + '様、今日もお会いできてうれしいです。'
     rescue
-      puts '先に無脳ちゃんに名前をつけてあげてください。'
+      puts 'まず無脳ちゃんに名前をつけてあげてください。'
+      meimei
+      puts @names['yome']+'> '+@names['master'] + '様、お会いできてうれしいです。よろしくお願いします。'
     end
   end
+
+#  def talk
+#    talk=[[Shiritori,'しりとりをしましょう。'],
+#          [Learning,'何か言葉を教えてください。'],
+#          [Shiritori_Learning,'しりとりに出てきた言葉の意味を教えてください。'],
+#          [Shittakaburi,'言葉の意味を教えてあげましょう。']
+#        ]
+#    puts @names['yome']+'> それでは、'+talk[rand(4)][1]
+#  return  Shittakaburi.new
+#  end
 end
 
-Greeting.new
+session=Greeting.new
+# session.talk
--- meimei.rb
+++ meimei.rb
@@ -1,14 +1,16 @@
 require 'yaml'
 require 'pp'
 
-names={}
+def meimei
+  names={}
 
-puts '私の名前は？'
-names['yome']=gets.chomp
-puts '私の名前は ' + names['yome'] + ' っていうの？'
+  puts '私の名前は？'
+  names['yome']=gets.chomp
+  puts '私の名前は ' + names['yome'] + ' っていうの？'
 
-puts 'あなたをなんと呼べばいいの？'
-names['master']=gets.chomp
-puts names['master'] + ' でいいのね？'
+  puts 'あなたをなんと呼べばいいの？'
+  names['master']=gets.chomp
+  puts names['master'] + ' でいいのね？'
 
-YAML.dump(names,File.open("NAMES.yaml", "w"))
+  YAML.dump(names,File.open("NAMES.yaml", "w"))
+end
