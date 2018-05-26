a5055e58db006d3aed84a3d68ce6fd6c27b11e27
tapaboy <tapaboy.shadowmoon@gmail.com>
2018-05-26 20:15:25 +0900

    会話（？）内で式展開できるようにして、少しスッキリさせた。 ランダム会話に備えて、裏会話作成。

--- learning.rb
+++ learning.rb
@@ -14,16 +14,14 @@ class Learning
   end
 
   def ask
-    puts '何か言葉を教えて。'
+#    puts '何か言葉を教えて。'
     print '単語> '
     @word=gets.chomp
     print '意味> '
     @mean=gets.chomp
-    puts '「' + @word + '」とは「' + @mean + '」という意味なのね。覚えたわ。'
-#    @dict_value = []
-#    @dict_value = @dictionary[@word]
-#    @dict_value.push = @mean
-#    puts @dict_value
+    puts "「#{@word}」とは「#{@mean}」という意味なのね。覚えたわ。"
+    ura_strings = "「いいこと？#{@word}ていうのはね#{@mean}という意味なのよ。これくらい常識よ。覚えておきなさい。"
+    
     @dictionary[@word] = [@dictionary[@word], @mean].flatten.compact
     YAML.dump(@dictionary,File.open('DICTIONARY.yaml', 'w'))
   end
