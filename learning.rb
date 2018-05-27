91a3fd0fb45037896bfd3439e745f8c2fb20fc24
tapaboy <tapaboy.shadowmoon@gmail.com>
2018-05-27 14:36:20 +0900

    Shiritori_LearningをLearningのサブクラスにして、コードを完結にした。

--- learning.rb
+++ learning.rb
@@ -7,21 +7,19 @@ class Learning
 
     begin
       @dictionary=YAML.load_file'DICTIONARY.yaml'
-#      pp @dictionary
     rescue
       @dictionary={}
     end
   end
 
   def ask
-#    puts '何か言葉を教えて。'
     print '単語> '
     @word=gets.chomp
     print '意味> '
     @mean=gets.chomp
     puts "「#{@word}」とは「#{@mean}」という意味なのね。覚えたわ。"
     ura_strings = "「いいこと？#{@word}ていうのはね#{@mean}という意味なのよ。これくらい常識よ。覚えておきなさい。"
-    
+
     @dictionary[@word] = [@dictionary[@word], @mean].flatten.compact
     YAML.dump(@dictionary,File.open('DICTIONARY.yaml', 'w'))
   end
--- shiritori_learning.rb
+++ shiritori_learning.rb
@@ -1,19 +1,18 @@
 require 'yaml'
-#require './learning'
+require './learning'
 require 'pp'
 
-class Shiritori_Learning
+class Shiritori_Learning < Learning
   def initialize
-    @names=YAML.load_file'NAMES.yaml'
+    @names = YAML.load_file'NAMES.yaml'
 
-#    super
     begin
-      @dictionary=YAML.load_file'DICTIONARY.yaml'
+      @dictionary = YAML.load_file'DICTIONARY.yaml'
     rescue
-      @dictionary={}
+      @dictionary = {}
     end
     begin
-      @pc_wordsbank=YAML.load_file'WORDSBANK.yaml'
+      @pc_wordsbank = YAML.load_file'WORDSBANK.yaml'
     rescue
       puts "#{Yome}> まだしりとりやったことないから、言葉を知らないの。"
       exit
@@ -22,14 +21,8 @@ class Shiritori_Learning
 
   def ask
     #### しりとりデータベースからランダムに言葉を取り出す。
-    select=rand(@pc_wordsbank.size)
+    select = rand(@pc_wordsbank.size)
     puts "「#{@pc_wordsbank[select]}」てどういう意味なの？"
-    print '単語> '
-    @word=gets.chomp
-    print '意味> '
-    @mean=gets.chomp
-    puts "「#{@word}」とは「#{@mean}」という意味なのね。覚えたわ。"
-    @dictionary[@word] = [@dictionary[@word], @mean].flatten.compact
-    YAML.dump(@dictionary,File.open('DICTIONARY.yaml', 'w'))
+    super  #Learningのaskメソッドを活用
   end
 end
